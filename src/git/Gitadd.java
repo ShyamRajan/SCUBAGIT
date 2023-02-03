c8c6a82f71b21e640ffc000a75f991fe003f1e1a
Shyam Rajan <shyam12shyam@gmail.com>
2023-02-03 18:10:45 +0530

    Update Gitadd.java
    changed a = 2 and a+b

--- src/git/Gitadd.java
+++ src/git/Gitadd.java
@@ -4,9 +4,9 @@ public class Gitadd {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 10;
+		int a = 2;
 		int b = 10;
-		System.out.println(a-b);
+		System.out.println(a+b);
 
 	}
 
